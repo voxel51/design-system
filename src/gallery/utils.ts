f323ed4cb1ea4334446b63ff4639e62ce2f63e15
Tom Schmidt <tom.schmidt@voxel51.com>
2025-09-29 17:04:24 -0700

    add static text for better style comparison

--- src/gallery/utils.ts
+++ src/gallery/utils.ts
@@ -1,3 +1,6 @@
-import { loremIpsum } from "lorem-ipsum";
+import { ILoremIpsumParams, loremIpsum } from "lorem-ipsum";
 
-export const generateText = () => loremIpsum();
+export const generateText = () =>
+  "The quick brown fox jumps over the lazy dog 1,234,567,890";
+
+export const lorem = (params?: ILoremIpsumParams) => loremIpsum(params);
