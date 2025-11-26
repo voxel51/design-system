9390e54f9898f61eb2c41f566d1c49ae88b2974d
Tom Schmidt <tom.schmidt@voxel51.com>
2025-11-26 09:29:00 -0800

    add basic tests for button interaction

--- src/components/Button/Button.spec.tsx
+++ src/components/Button/Button.spec.tsx
@@ -1,9 +1,36 @@
 import { render, screen } from "@testing-library/react";
 import { Button } from "./Button";
+import userEvent, { UserEvent } from "@testing-library/user-event";
 
 describe("Button", () => {
   it("renders with text", () => {
     render(<Button>Click me</Button>);
     expect(screen.getByText("Click me")).toBeInTheDocument();
+  });
+
+  describe("onClick", () => {
+    let onClick: jest.Func;
+    let user: UserEvent;
+
+    beforeEach(() => {
+      onClick = jest.fn();
+      user = userEvent.setup();
+    });
+
+    it("should fire when clicked", async () => {
+      render(<Button onClick={onClick}></Button>);
+      const button = screen.getByRole("button");
+
+      await user.click(button);
+      expect(onClick).toHaveBeenCalled();
+    });
+
+    it("should not fire when disabled", async () => {
+      render(<Button onClick={onClick} disabled></Button>);
+      const button = screen.getByRole("button");
+
+      await user.click(button);
+      expect(onClick).not.toHaveBeenCalled();
+    });
   });
 });
