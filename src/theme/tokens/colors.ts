f2806ceabd4a45c727c5a77e3ca742f5d1ad90d4
Tom Schmidt <tom.schmidt@voxel51.com>
2025-11-26 15:03:42 -0800

    add support for toggling light/dark theme

--- src/theme/tokens/colors.ts
+++ src/theme/tokens/colors.ts
@@ -49,12 +49,20 @@ export const colors = {
   light: {
     content: {
       background: {
+        primary: "#FFF",
+        secondary: "#AAA",
+      },
+      text: {
         primary: "#000",
         secondary: "#555",
       },
-      text: {
-        primary: "#FFF",
-        secondary: "#AAA",
+      border: {
+        secondary: {
+          primary: "#333",
+          hover: "#555",
+          active: "#333",
+          disabled: "#444",
+        },
       },
     },
     action: {
@@ -62,13 +70,13 @@ export const colors = {
         primary: "#FF6D05FF",
         hover: "#D05700FF",
         active: "#9C4100FF",
-        text: "#FFF",
+        text: "#000",
       },
       secondary: {
-        primary: "transparent",
-        hover: "transparent",
-        active: "#222",
-        text: "#FFF",
+        primary: "#FFF",
+        hover: "#FF6D05FF",
+        active: "#9C4100FF",
+        text: "#000",
       },
       success: {
         primary: "#047A48FF",
