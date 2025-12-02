3b1e002a57a1a6ea3712f3aa41f3b755a7b8165e
Tom Schmidt <tom.schmidt@voxel51.com>
2025-12-01 16:48:04 -0800

    implement basic text component

---
+++ src/components/Text/Text.tsx
@@ -0,0 +1,37 @@
+import type { FC, HTMLAttributes } from "react";
+import clsx from "clsx";
+
+export type TextVariant = "h1" | "h2" | "h3" | "h4" | "base" | "subtext";
+
+export interface TextProps extends HTMLAttributes<any> {
+  variant?: TextVariant;
+}
+
+const variantStyles: Record<TextVariant, string> = {
+  h1: "text-2xl",
+  h2: "text-xl",
+  h3: "text-lg",
+  h4: "text-md",
+  base: "text-base",
+  subtext: "text-sm",
+};
+
+export const Text: FC<TextProps> = ({
+  variant = "base",
+  children,
+  className,
+  ...props
+}) => {
+  return (
+    <span
+      className={clsx(
+        "text-content-text-primary",
+        variantStyles[variant],
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </span>
+  );
+};
