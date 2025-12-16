5776fc94a4e732bc016d8b7b035f2a9d7ed9440f
Erik <erik@voxel51.com>
2025-12-16 10:29:20 -0500

    add unit test for pill isStatus

--- src/components/Pill/Pill.spec.tsx
+++ src/components/Pill/Pill.spec.tsx
@@ -20,4 +20,33 @@ describe("Pill", () => {
     expect(pill).toHaveAttribute("id", "test-pill");
     expect(pill).toHaveAttribute("data-testid", "custom-pill");
   });
+
+  it("should render status icon when isStatus is true", () => {
+    render(<Pill isStatus={true}>{pillText}</Pill>);
+
+    const pill = screen.getByText(pillText);
+    const svg = pill.querySelector("svg");
+    const circle = pill.querySelector("circle");
+
+    expect(svg).toBeInTheDocument();
+    expect(circle).toBeInTheDocument();
+  });
+
+  it("should not render status icon when isStatus is false", () => {
+    render(<Pill isStatus={false}>{pillText}</Pill>);
+
+    const pill = screen.getByText(pillText);
+    const svg = pill.querySelector("svg");
+
+    expect(svg).not.toBeInTheDocument();
+  });
+
+  it("should not render status icon when isStatus is not provided", () => {
+    render(<Pill>{pillText}</Pill>);
+
+    const pill = screen.getByText(pillText);
+    const svg = pill.querySelector("svg");
+
+    expect(svg).not.toBeInTheDocument();
+  });
 });
