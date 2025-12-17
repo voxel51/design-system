5a57f9dc9f44faa8941a3b71236e1973032c9d5d
Erik <erik@voxel51.com>
2025-12-17 10:14:06 -0500

    add Partial to satisfy only some enums being implemented

--- src/components/Badge/Badge.tsx
+++ src/components/Badge/Badge.tsx
@@ -10,9 +10,9 @@ import {
   TextColor,
   TextColorProp,
 } from "@/types";
+import { cn } from "@/util/classes";
 import clsx from "clsx";
 import type { FC, HTMLAttributes } from "react";
-import { cn } from "@/util/classes";
 
 export interface BadgeProps extends HTMLAttributes<HTMLSpanElement> {
   size?: Size;
@@ -22,7 +22,7 @@ export interface BadgeProps extends HTMLAttributes<HTMLSpanElement> {
   backgroundColor?: BackgroundColorProp;
 }
 
-const sizeStyles: Record<Size, string> = {
+const sizeStyles: Partial<Record<Size, string>> = {
   [Size.Xs]: clsx("px-2.5 py-0.75", "text-xs/5"),
   [Size.Sm]: clsx("px-3.5 py-1.5", "text-sm/5"),
   [Size.Md]: clsx("px-4 py-2", "text-md/5"),
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,8 +1,8 @@
 import { Size, Variant } from "@/types";
+import { cn } from "@/util/classes";
 import { Button as HeadlessButton } from "@headlessui/react";
 import clsx from "clsx";
-import type { FC, ButtonHTMLAttributes } from "react";
-import { cn } from "@/util/classes";
+import type { ButtonHTMLAttributes, FC } from "react";
 
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: Variant;
@@ -41,13 +41,13 @@ const variantStyles: Record<Variant, string> = {
   ),
 };
 
-const sizeStyles: Record<Size, string> = {
+const sizeStyles: Partial<Record<Size, string>> = {
   [Size.Xs]: clsx("px-2.5 py-0.75", "text-xs/5"),
   [Size.Sm]: clsx("px-3.5 py-1.5", "text-sm/5"),
   [Size.Md]: clsx("px-4 py-2", "text-md/5"),
 };
 
-const iconStyles: Record<Size, string> = {
+const iconStyles: Partial<Record<Size, string>> = {
   [Size.Xs]: clsx("w-4 h-4", "leading-none"),
   [Size.Sm]: clsx("w-4 h-4", "leading-none"),
   [Size.Md]: clsx("w-5 h-5", "leading-none"),
