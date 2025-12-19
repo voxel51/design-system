aadb8e6dab4014fac1e0f805b5ebc199e5b4ba71
Erik <erik@voxel51.com>
2025-12-19 08:15:04 -0500

    resolve unit tests, remove text style unit test

--- src/components/Input/InputIcon.spec.tsx
+++ src/components/Input/InputIcon.spec.tsx
@@ -11,4 +11,22 @@ describe("InputIcon", () => {
     const svg = container.querySelector("svg");
     expect(svg).toBeInTheDocument();
   });
+
+  it("should apply text-content-text-primary when hasText is true", () => {
+    const { container } = render(
+      <InputIcon Icon={CheckmarkIcon} size={Size.Md} hasText={true} />
+    );
+    const span = container.querySelector("span");
+    expect(span).toHaveClass("text-content-text-primary");
+    expect(span).not.toHaveClass("text-content-text-secondary");
+  });
+
+  it("should apply text-content-text-secondary when hasText is false", () => {
+    const { container } = render(
+      <InputIcon Icon={CheckmarkIcon} size={Size.Md} hasText={false} />
+    );
+    const span = container.querySelector("span");
+    expect(span).toHaveClass("text-content-text-secondary");
+    expect(span).not.toHaveClass("text-content-text-primary");
+  });
 });
