d92116742c91eaf861c95994721cafa7666951b1
Tom Schmidt <tom.schmidt@voxel51.com>
2026-03-23 10:19:13 -0700

    add align and justify properties for common stack content alignment

--- src/components/Stack/Stack.tsx
+++ src/components/Stack/Stack.tsx
@@ -1,12 +1,30 @@
 import { FC, HTMLAttributes } from "react";
 
-import { Orientation, Spacing } from "@/types";
+import { Align, Justify, Orientation, Spacing } from "@/types";
 import { cn } from "@/util/classes";
 
 export interface StackProps extends HTMLAttributes<HTMLDivElement> {
+  align?: Align;
+  justify?: Justify;
   orientation?: Orientation;
   spacing?: Spacing;
 }
+
+const alignStyles: Record<Align, string> = {
+  [Align.Baseline]: "items-baseline",
+  [Align.Center]: "items-center",
+  [Align.End]: "items-end",
+  [Align.Start]: "items-start",
+};
+
+const justifyStyles: Record<Justify, string> = {
+  [Justify.Around]: "justify-around",
+  [Justify.Between]: "justify-between",
+  [Justify.Center]: "justify-center",
+  [Justify.End]: "justify-end",
+  [Justify.Evenly]: "justify-evenly",
+  [Justify.Start]: "justify-start",
+};
 
 const spacingStyles: Record<Spacing, string> = {
   [Spacing.None]: "",
@@ -38,6 +56,17 @@ const spacingStyles: Record<Spacing, string> = {
  * </Stack>
  * ```
  *
+ * @example
+ * ```tsx
+ * <Stack orientation={Orientation.Horizontal} align={Align.Center} justify={Justify.Between}>
+ *   <Text>Left</Text>
+ *   <Text>Middle</Text>
+ *   <Text>Right</Text>
+ * </Stack>
+ * ```
+ *
+ * @param align Alignment of children along the stack's cross axis. See {@link Align}.
+ * @param justify Justification of children along the stack's primary axis. See {@link Justify}.
  * @param orientation Orientation of the stack. See {@link Orientation}.
  * @param spacing Spacing between child components in the stack. See {@link Spacing}.
  * @param children Content which is wrapped by this component.
@@ -45,6 +74,8 @@ const spacingStyles: Record<Spacing, string> = {
  * @param props Additional HTML properties to apply to the component.
  */
 export const Stack: FC<StackProps> = ({
+  align,
+  justify,
   orientation = Orientation.Row,
   spacing = Spacing.None,
   children,
@@ -57,6 +88,8 @@ export const Stack: FC<StackProps> = ({
         "flex",
         orientation === Orientation.Column && "flex-col",
         spacingStyles[spacing],
+        align && alignStyles[align],
+        justify && justifyStyles[justify],
         className
       )}
       {...props}
---
+++ src/types/justify.ts
@@ -0,0 +1,8 @@
+export enum Justify {
+  Around = "around",
+  Between = "between",
+  Center = "center",
+  End = "end",
+  Evenly = "evenly",
+  Start = "start",
+}
