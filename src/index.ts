3d132e9f54c8fc409f25ad66a87a106d9a758f79
Mike OBrien <michaelobriena@gmail.com>
2026-05-04 09:57:20 -0500

    feat: add useDisclosure hook
    Headless controlled/uncontrolled open state primitive with toggle callback.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 export * from "./components";
 export * from "./theme";
 export * from "./types";
+export { useDisclosure } from "./util/useDisclosure";
+export type { UseDisclosureOptions, UseDisclosureReturn } from "./util/useDisclosure";
 
 import "./styles/globals.css";
